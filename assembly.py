7c153cfb9eef5f746635b8021fc1a8abf8714876
Amon <j.niedzw@gmail.com>
2021-01-19 05:40:00 +0100

    [Greedy] Iterative greedy solution v3 - fix issue with suffixes - change stop loop condition - code cleanup and refactoring - final results uploaded

--- assembly.py
+++ assembly.py
@@ -27,38 +27,14 @@ def sim_score(s1: str, s2: str) -> float:
     return score / len(s1)
 
 
-def exact(s1: str, s2: str) -> float:
-    if s1 == s2:
-        return 1.0
-    else:
-        return 0.0
-
-
-def check_exact_longest(s1: str, s2: str) -> str:
-    i = s1.find(s2[0])
-    while i >= 0:
-        s1_post = s1[i:]
-        s2_pre = s2[:len(s1_post)]
-        if s1_post == s2_pre:
-            return s1_post
-
-        i = s1.find(s2[0], i + 1)
-
-    return ''
-
-
-def threshold_len_exact(len1: int, len2: int) -> bool:
-    return (
-        abs(len1 - len2) < 4 and
-        len2 > 0
-    )
-# max_exact = check_exact_longest(s1, s2)
-# if threshold_len_exact(len(s1_post), len(max_exact)):
-#     return max_exact
+def sim_exact(s1: str, s2: str) -> float:
+    return 1.0 if s1 == s2 else 0.0
 
 
 SIM_SCORE = 1.0
-SIM_FUNC = exact
+SIM_FUNC = sim_exact
+START_SEQ_LEN = 0
+FIX_LONG = 4
 
 
 class Seq:
@@ -69,10 +45,8 @@ class Seq:
         self.id = Seq.seq_id
         Seq.seq_id += 1
 
-    def _single_longest_prefix(self, sec_seq: 'Seq'):
-        s1 = self.seq
-        s2 = sec_seq.seq
-
+    @staticmethod
+    def get_longest_fix(s1: str, s2: str):
         i = s1.find(s2[0])
         while i >= 0:
             s1_post = s1[i:]
@@ -84,35 +58,21 @@ class Seq:
 
         return ''
 
-    def _single_longest_suffix(self, sec_seq: 'Seq'):
-        s1 = sec_seq.seq
-        s2 = self.seq
-
-        i = s1.find(s2[0])
-        while i >= 0:
-            s1_post = s1[i:]
-            s2_pre = s2[:len(s1_post)]
-            if SIM_FUNC(s2_pre, s1_post) >= SIM_SCORE:
-                return s1_post
-
-            i = s1.find(s2[0], i + 1)
-
-        return ''
-
-    def longest_fix(self, seqs: Dict[int, 'Seq']) -> (int, str, str):
-        lp = ''
+    @staticmethod
+    def _longest_fix(sel_seq: 'Seq', seqs: Dict[int, 'Seq']) -> (int, str, str):
         lp_seq_id = -1
+        lp = ''
         fix = ''
 
         for seq_id, seq in seqs.items():
-            slp = self._single_longest_prefix(seq)
+            slp = Seq.get_longest_fix(sel_seq.seq, seq.seq)
             if len(slp) > len(lp):
                 lp = slp
                 lp_seq_id = seq_id
                 fix = 'prefix'
 
         for seq_id, seq in seqs.items():
-            slp = self._single_longest_suffix(seq)
+            slp = Seq.get_longest_fix(seq.seq, sel_seq.seq)
             if len(slp) > len(lp):
                 lp = slp
                 lp_seq_id = seq_id
@@ -121,17 +81,20 @@ class Seq:
         return lp_seq_id, lp, fix
 
     def merged_best_seq(self, seqs: Dict[int, 'Seq']) -> 'Seq':
-        best_seq_id, longest_prefix, fix = self.longest_fix(seqs)
-        if best_seq_id != -1:
-            best_seq = seqs[best_seq_id]
-            self.id = best_seq_id
+        best_seq_id, longest_fix, fix = Seq._longest_fix(self, seqs)
+        if len(longest_fix) < START_SEQ_LEN // FIX_LONG or best_seq_id < 0:
+            return self
 
+        best_seq = seqs[best_seq_id]
+        self.id = best_seq_id
+
+        print(f'Longest fix: {fix}, {len(longest_fix)}')
         if fix == 'prefix':
-            self.seq = f'{self.seq}{best_seq.seq[len(longest_prefix):]}'
+            self.seq = f'{self.seq}{best_seq.seq[len(longest_fix):]}'
         elif fix == 'suffix':
-            self.seq = f'{best_seq.seq[:len(longest_prefix)]}{self.seq}'
+            self.seq = f'{best_seq.seq[:-len(longest_fix)]}{self.seq}'
         else:
-            print(f'[*] Skip seq with length = {len(self.seq)}')
+            raise Exception('Wrong path')
 
         return self
 
@@ -143,31 +106,25 @@ if __name__ == '__main__':
     parser.add_argument('-o', '--output', type=str, help='output fasta')
     args = parser.parse_args()
 
-    # if args.score < 1.0:
-    #     SIM_SCORE = args.score
-    #     SIM_FUNC = sim_score
-
     seqs = [Seq(s) for s in read_sequences(args.file)]
+    START_SEQ_LEN = len(seqs[0].seq)
     seqs = {s.id: s for s in seqs}
 
     run = 1
     while len(seqs) > 1:
-        p_seqs_len = len(seqs)
         seqs_keys = list(seqs.keys())
         print(f'[*] Run = {run}; keys = {len(seqs_keys)}')
-        for i, s_id in enumerate(seqs_keys):
-            if i & 0xf == 0:
-                print(f'[*] Done keys = {i}')
+        for k, s_id in enumerate(seqs_keys):
+            if k & 0xff == 0:
+                print(f'[*] Done keys = {k}, seqs left = {len(seqs)}')
+                pass
             if s_id in seqs:
                 start_seq_obj: Seq = seqs.pop(s_id)
-                seq_obj: Seq = start_seq_obj.merged_best_seq(seqs)
+                seq_obj = start_seq_obj.merged_best_seq(seqs)
                 seqs[seq_obj.id] = seq_obj
         run += 1
-
         SIM_SCORE *= 0.8
         SIM_FUNC = sim_score
-        if p_seqs_len == len(seqs):  # nothing changed, merge all left sequences
-            break
 
     super_seq = ''.join(s.seq for s in seqs.values())
     print(f'[+] Done, super sequence length = {len(super_seq)}')
@@ -175,5 +132,5 @@ if __name__ == '__main__':
     filename = args.output if args.output else f'training/super.fasta'
 
     with open(filename, 'w') as rf:
-        rf.write('>a\n')
+        rf.write('>super_sequence\n')
         rf.write(super_seq)
